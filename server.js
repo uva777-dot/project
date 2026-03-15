e1ef3714501ebcd844da00712a74d3d3dbe21a64
uva777-dot <uva777@gmail.com>
2026-03-15 03:16:08 +0000

    created server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://16.112.189.57:${port}`);
+});
